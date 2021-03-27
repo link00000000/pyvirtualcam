749a0b2c4d0e33a3d466efd0cb32913ac34e6161
Maik Riechert <maik.riechert@arcor.de>
2021-03-27 10:06:09 +0000

    try to disable some kernel modules

--- .github/scripts/install-v4l2loopback.sh
+++ .github/scripts/install-v4l2loopback.sh
@@ -10,6 +10,17 @@ set -e -x
 sudo apt-get install -y patchutils libproc-processtable-perl
 git clone --depth=1 git://linuxtv.org/media_build.git
 cd media_build 
+# disable some kernel options that cause build failures
+disable=(
+    "VIDEO_VIA_CAMERA"
+    "VIDEO_OV9650"
+    "SOC_CAMERA"
+)
+after_line="# These options should default to off"
+for opt in "${disable[@]}"; do
+    sed -i "/^$after_line/a disable_config('$opt');" v4l/scripts/make_kconfig.pl
+done
+tail -n 100 v4l/scripts/make_kconfig.pl
 ./build
 sudo make install
 
